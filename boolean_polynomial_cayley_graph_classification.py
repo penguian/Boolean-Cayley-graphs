c983b2f0b0fb818a6bb30ca929047e7eda029e80
Paul Leopardi <leopardi@users.sourceforge.net>
2016-09-26 08:15:37 +1000

    Use BentFunction. Use dillon_schatz_design_matrix rather than dillon_schatz_incidence_structure. Check that weight_class_matrix == dillon_schatz_design_matrix.

--- boolean_polynomial_cayley_graph_classification.py
+++ boolean_polynomial_cayley_graph_classification.py
@@ -8,45 +8,53 @@
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
 
+from sage.combinat.designs.incidence_structures import IncidenceStructure
 from sage.crypto.boolean_function import BooleanFunction
 from sage.structure.sage_object import SageObject, load
 
-from cayley_graph_classification import cayley_graph_classification
-from dillon_schatz_incidence_structure import dillon_schatz_incidence_structure
-from strongly_regular_graph import StronglyRegularGraph
+from bent_function import BentFunction
+from bent_function_cayley_graph_classification import BentFunctionCayleyGraphClassification
 
 import cayley_graph_controls as controls
 
 
 class BooleanPolynomialCayleyGraphClassification(SageObject):
 
+
     def __init__(self, p):
+        r"""
+        """
         verbose = controls.verbose
 
         self.boolean_polynomial = p
 
-        f = BooleanFunction(p)
+        f = BentFunction(p)
 
-        n = f.algebraic_normal_form()
-        self.algebraic_normal_form = n
+        self.algebraic_normal_form = f.algebraic_normal_form()
 
-        G, D, I = dillon_schatz_incidence_structure(f)
-        self.cayley_incidence_structure = G
-        self.dillon_schatz_design_matrix = D
-        self.dillon_schatz_incidence_structure = I
+        self.dillon_schatz_design_matrix = f.dillon_schatz_design_matrix()
 
-        fcc, srgs = cayley_graph_classification(f)
-        self.cayley_graph_class_list = srgs
+        fcc = BentFunctionCayleyGraphClassification(f)
+        self.cayley_graph_class_list   = fcc.cayley_graph_class_list
         self.cayley_graph_index_matrix = fcc.cayley_graph_index_matrix
+        weight_class_matrix            = fcc.weight_class_matrix
+        if weight_class_matrix != self.dillon_schatz_design_matrix:
+            raise ValueError, ("weight_class_matrix != self.dillon_schatz_design_matrix" + "\n"
+                               + str(weight_class_matrix) + "\n"
+                               + str(self.dillon_schatz_design_matrix))
         if verbose:
             self.__print__()
 
 
     def mangled_name(self, name):
+        r"""
+        """
         return self.__class__.__name__ + "__" + name
 
 
     def __print__(self):
+        r"""
+        """
         p = self.boolean_polynomial
         print "Boolean polynomial:   ", p
 
@@ -56,31 +64,33 @@ class BooleanPolynomialCayleyGraphClassification(SageObject):
         print "Algebraic normal form:", n
         print "Function", ("is" if f.is_bent() else "is not"), "bent."
 
-        G = self.cayley_incidence_structure
         D = self.dillon_schatz_design_matrix
-        I = self.dillon_schatz_incidence_structure
-        print "Dillon Schatz design",
-        print ("is" if I.is_isomorphic(G) else "is not"),
-        print "isomorphic to Cayley graph incidence structure."
+        I = IncidenceStructure(D)
         print "Incidence structure t-design parameters:",
         print I.is_t_design(return_parameters=True)
 
         srgs = self.cayley_graph_class_list
-        print "Strongly regular parameters, rank, order",
-        print "and clique polynomial"
+        print "Clique polynomial,",
+        print "strongly regular parameters, rank, and order",
         print "of each representative Cayley graph",
         print "in the extended affine class:"
         for s in srgs:
+            print s.clique_polynomial
             print s.strongly_regular_parameters,
-            print s.rank, s.group_order, s.clique_polynomial
+            print s.rank, s.group_order
+            print ""
         print "Cayley graph representative index matrix:"
         print self.cayley_graph_index_matrix
 
 
 def save_cayley_graph_classification(c, name):
+    r"""
+    """
     c.save(c.mangled_name(name))
 
 
 def load_cayley_graph_classification(name):
+    r"""
+    """
     mangled_name = "BooleanPolynomialCayleyGraphClassification" + "__" + name
     return load(mangled_name)
