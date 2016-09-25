f465172d867e132aa2b673e938eca6b7681cefd1
Paul Leopardi <leopardi@users.sourceforge.net>
2016-09-25 23:48:13 +1000

    First draft of weight_class function.

---
+++ weight_class.py
@@ -0,0 +1,18 @@
+
+#*****************************************************************************
+#       Copyright (C) 2016 Paul Leopardi paul.leopardi@gmail.com
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#  as published by the Free Software Foundation; either version 2 of
+#  the License, or (at your option) any later version.
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
+
+from sage.functions.other import sqrt
+
+
+def weight_class(length, weight):
+    r"""
+    """
+    sqrtlength = sqrt(length)
+    return ((weight*2)/sqrtlength - sqrtlength + 1) / 2
