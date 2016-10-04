1b72086c1e6019b3b3291005fb98f59e6d773e31
Paul Leopardi <leopardi@users.sourceforge.net>
2016-10-04 22:23:27 +1100

    A mixin class with methods that load and save SageObject objects with standardized names.

---
+++ persistent.py
@@ -0,0 +1,48 @@
+r"""
+A mixin class with methods that load and save SageObject objects with standardized names.
+
+
+AUTHORS:
+
+- Paul Leopardi (2016-08-04): initial version
+
+"""
+
+#*****************************************************************************
+#       Copyright (C) 2016 Paul Leopardi paul.leopardi@gmail.com
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#  as published by the Free Software Foundation; either version 2 of
+#  the License, or (at your option) any later version.
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
+
+
+from sage.structure.sage_object import load
+
+
+class Persistent(object):
+    r"""
+    """
+
+
+    @classmethod
+    def mangled_name(cls, name):
+        r"""
+        """
+        return cls.__name__ + "__" + name
+
+
+    @classmethod
+    def load_mangled(cls, name):
+        r"""
+        """
+        return load(cls.mangled_name(name))
+
+
+    def save_mangled(self, name):
+        r"""
+        """
+        self.save(self.__class__.mangled_name(name))
+
+
