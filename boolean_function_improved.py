b3cc295129e0aec5561caad38386b09139a4f04f
Paul Leopardi <leopardi@users.sourceforge.net>
2016-10-12 23:53:04 +1100

    Add linear_code() method.

--- boolean_function_improved.py
+++ boolean_function_improved.py
@@ -17,16 +17,25 @@ AUTHORS:
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
 
+from sage.arith.srange import xsrange
+from sage.coding.linear_code import LinearCode
 from sage.crypto.boolean_function import BooleanFunction
+from sage.matrix.constructor import matrix
+from sage.rings.finite_rings.finite_field_constructor import FiniteField as GF
 
 from boolean_cayley_graph import boolean_cayley_graph
 from integer_bits import inner
 from persistent import Persistent
 
 
+base2 = lambda dim, num: num.digits(2, padto=dim)
+
+
 class BooleanFunctionImproved(BooleanFunction, Persistent):
     r"""
     """
+
+
     def cayley_graph(self):
         r"""
         """
@@ -44,10 +53,23 @@ class BooleanFunctionImproved(BooleanFunction, Persistent):
 
         $x \mapsto \mathtt{self}(x + b) + \langle c, x \rangle + d$.
         """
-        base2 = lambda dim, num: num.digits(2, padto=dim)
-
         dim = self.nvariables()
         return lambda x: self(base2(dim, x ^ b)) ^ (0 if c == 0 else inner(c, x)) ^ d
+
+
+    def linear_code(self):
+        r"""
+        """
+        dim = self.nvariables()
+        n = 2 ** dim
+        f = self.extended_translate()
+        support = [y
+                   for y in xsrange(n)
+                   if f(y)==1]
+        M = matrix(GF(2), [[inner(x, y)
+                            for y in support]
+                            for x in xsrange(n)])
+        return LinearCode(M)
 
 
     def weight(self):
