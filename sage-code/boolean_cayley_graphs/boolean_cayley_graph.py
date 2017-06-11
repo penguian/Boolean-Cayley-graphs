31752ffeac7f67462dbfd6fc928c9395dd3b8ad4
Paul Leopardi <leopardi@users.sourceforge.net>
2017-06-11 20:50:10 +1000

    Document as per Sage General Conventions.

--- sage-code/boolean_cayley_graphs/boolean_cayley_graph.py
+++ sage-code/boolean_cayley_graphs/boolean_cayley_graph.py
@@ -1,11 +1,28 @@
-"""
+r"""
 Cayley graph of a Boolean function.
 
-Paul Leopardi.
-"""
+AUTHORS:
 
+- Paul Leopardi (2016-11-23): initial version
+
+EXAMPLE:
+
+::
+
+    sage: from boolean_cayley_graphs.boolean_cayley_graph import boolean_cayley_graph
+    sage: f = lambda n: (n // 2) % 2
+    sage: [f(i) for i in range(4)]
+    [0, 0, 1, 1]
+    sage: g = boolean_cayley_graph(2, f)
+    sage: g.adjacency_matrix()
+
+    [0 0 1 1]
+    [0 0 1 1]
+    [1 1 0 0]
+    [1 1 0 0]
+"""
 #*****************************************************************************
-#       Copyright (C) 2016 Paul Leopardi paul.leopardi@gmail.com
+#       Copyright (C) 2016-2017 Paul Leopardi paul.leopardi@gmail.com
 #
 #  Distributed under the terms of the GNU General Public License (GPL)
 #  as published by the Free Software Foundation; either version 2 of
@@ -13,16 +30,51 @@ Paul Leopardi.
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
 
+
 from sage.arith.srange import srange
 from sage.graphs.graph import Graph
 
 
 def boolean_cayley_graph(dim, f):
     r"""
-    Given the non-negative number $m$ and the function `f`,
+    Construct the Cayley graph of a Boolean function.
+
+    Given the non-negative integer ``dim`` and the function ``f``,
     a Boolean function that takes a non-negative integer argument,
-    the function `Boolean_Cayley_graph` constructs the Cayley graph of
-    `f` as a Boolean function on $\mathbb{Z}_2^d$, with the canonical ordering.
-    The value `f(0)` is assumed to be 0, so the graph is always simple.
+    the function ``Boolean_Cayley_graph`` constructs the Cayley graph of ``f``
+    as a Boolean function on :math:`\mathbb{Z}_2^{dim}`, with the canonical ordering.
+    The value ``f(0)`` is assumed to be ``0``, so the graph is always simple.
+
+    INPUT:
+
+    - ``dim`` -- integer. The Boolean dimension of the given function.
+    - ``f`` -- function. A Boolean function expressed as a Python function taking
+      non-negative integer arguments.
+
+    OUTPUT:
+
+    A ``Graph`` object representing the Cayley graph of ``f``.
+
+    .. SEEALSO:
+        :module:`sage.graphs.graph`
+
+    EXAMPLES:
+
+    The Cayley graph of the function ``f`` where :math:`f(n) = n \mod 2`.
+
+    ::
+
+        sage: from boolean_cayley_graphs.boolean_cayley_graph import boolean_cayley_graph
+        sage: f = lambda n: n % 2
+        sage: g = boolean_cayley_graph(2, f)
+        sage: g.adjacency_matrix()
+
+        [0 1 0 1]
+        [1 0 1 0]
+        [0 1 0 1]
+        [1 0 1 0]
+
     """
-    return Graph([srange(2 ** dim), lambda i, j: f(i ^ j)], format="rule", immutable=True)
+    return Graph([srange(2 ** dim), lambda i, j: f(i ^ j)],
+                 format="rule",
+                 immutable=True)
