78dd7264d894847e18489085e40581d702da3293
Paul Leopardi <leopardi@users.sourceforge.net>
2017-10-13 00:08:38 +1100

    Tests for MPI parallel classification.

---
+++ sage-code/save_dim6_in_mpi_parallel.py
@@ -0,0 +1,29 @@
+r"""
+"""
+#*****************************************************************************
+#       Copyright (C) 2016 Paul Leopardi paul.leopardi@gmail.com
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#  as published by the Free Software Foundation; either version 2 of
+#  the License, or (at your option) any later version.
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
+
+from mpi4py import MPI
+from sage.all_cmdline import *
+
+from boolean_cayley_graphs.classify_in_mpi_parallel import save_classifications_in_parallel
+
+r"""
+"""
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+if rank == 0:
+    load("bent_function_extended_affine_representative_polynomials.sage")
+    list_of_f = bent_function_extended_affine_representative_polynomials_dimension_6()
+else:
+    list_of_f = None
+
+save_classifications_in_parallel(comm, 'p6_test_again', list_of_f, start=1)
+quit
