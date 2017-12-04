3c5e868e004d4ab14ee8eea9583b7cb28d236cea
Paul Leopardi <leopardi@users.sourceforge.net>
2017-12-04 23:00:33 +1100

    Re-create references as well as all API documentation. Bump documentation version to 0.0.1.1.

--- make-sphinx-apidoc.sh
+++ make-sphinx-apidoc.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
+#
+# To execute this script, use "sage -sh"
+#
+# To upload updated apidoc to leopardi@SourceForge (2017), use:
+# rsync -avz --delete doc/_build/html/ leopardi@web.sourceforge.net:/home/project-web/boolean-cayley-graphs/htdocs/
+#
 BCG_VERSION=${1:-"0.0"}
-BCG_RELEASE=${2:-${BCG_VERSION}.1}
-sphinx-apidoc -e -F -H "Boolean-Cayley-graphs" -A "Paul Leopardi" -V ${BCG_VERSION} -R ${BCG_RELEASE} -o doc sage-code
-cd doc
+BCG_RELEASE=${2:-${BCG_VERSION}.1.1}
+# Create doc/references.rst from sage-code/boolean_cayley_graphs/references.py
+pushd sage-code
+sage<<EOF
+from boolean_cayley_graphs.references import print_sage_references_index_rst
+ref_file = open("../doc/references.rst","w")
+print_sage_references_index_rst(file=ref_file)
+quit
+EOF
+popd
+pushd doc
+# Remove the old boolean_cayley_graphs module documentation.
+rm -rf boolean_cayley_graphs*.rst make.bat Makefile _*
+# Use sphinx-apidoc to re-create the documentation from the Python files in ../sage-code/boolean_cayley_graphs
+sphinx-apidoc -e -F -H "Boolean-Cayley-graphs" -A "Paul Leopardi" \
+    -V ${BCG_VERSION} -R ${BCG_RELEASE} \
+    -o . ../sage-code/boolean_cayley_graphs
 make html
+popd
