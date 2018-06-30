f1334e26feb4f165f8fcfe0ad17675283ac53092
Paul Leopardi <leopardi@users.sourceforge.net>
2018-06-30 22:29:15 +1000

    Improve documentation and doctests.

--- sage-code/boolean_cayley_graphs/graph_improved.py
+++ sage-code/boolean_cayley_graphs/graph_improved.py
@@ -24,6 +24,8 @@ class GraphImproved(Graph, Saveable):
     r"""
     A Graph and some of its computed properties, including its clique polynomial.
 
+    The constructor is based on the Graph constructor and takes the same arguments.
+
     EXAMPLES:
 
     ::
@@ -35,12 +37,28 @@ class GraphImproved(Graph, Saveable):
         sage: PI = GraphImproved(P)
         sage: PI.stored_clique_polynomial
         15*t^2 + 10*t + 1
-        sage: PI.save_mangled('PetersenGraph')
-        sage: LPI = GraphImproved.load_mangled('PetersenGraph')
+        sage: dir = tmp_dir()
+        sage: PI.save_mangled('PetersenGraph', directory=dir)
+        sage: LPI = GraphImproved.load_mangled('PetersenGraph', directory=dir)
         sage: LPI.stored_clique_polynomial
         15*t^2 + 10*t + 1
-        sage: GraphImproved.remove_mangled('PetersenGraph')
+        sage: GraphImproved.remove_mangled('PetersenGraph', directory=dir)
     """
     def __init__(self, graph=None, **kwargs):
+        r"""
+        Constructor, based on the Graph constructor.
+
+        TESTS:
+
+        ::
+
+            sage: c = graphs.ClebschGraph()
+            sage: c.clique_polynomial()
+            40*t^2 + 16*t + 1
+            sage: from boolean_cayley_graphs.graph_improved import GraphImproved
+            sage: ci = GraphImproved(c)
+            sage: ci.stored_clique_polynomial
+            40*t^2 + 16*t + 1
+        """
         Graph.__init__(self, graph, **kwargs)
         self.stored_clique_polynomial = graph.clique_polynomial()
