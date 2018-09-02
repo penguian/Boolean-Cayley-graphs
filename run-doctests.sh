8ee1ee6da4aa537fa44bccb451e1067de0de6a22
Paul Leopardi <leopardi@users.sourceforge.net>
2018-09-02 16:19:17 +1000

    Don't test the Postgres interface if Postgres is not running.

--- run-doctests.sh
+++ run-doctests.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 export SAGE_PATH=$(pwd)/sage-code
 export PYTHONPATH=$SAGE_PATH:$PYTHONPATH
-sage -t sage-code/boolean_cayley_graphs/*.py
+PACKAGE_DIR="sage-code/boolean_cayley_graphs"
+POSTGRES_MODULE="sage-code\/boolean_cayley_graphs\/classification_database_psycopg2.py"
+WHOLE_PACKAGE="$PACKAGE_DIR/*.py"
+PACKAGE_EXCEPT_POSTGRES=$(echo ${WHOLE_PACKAGE} | sed "s?$POSTGRES_MODULE ??")
+IS_POSTGRES_RUNNING="
+import psycopg2
+import sys
+try:
+    psycopg2.connect(dbname='postgres')
+except:
+    sys.exit(1)
+"
+if python -c "${IS_POSTGRES_RUNNING}"
+then
+    sage -t ${WHOLE_PACKAGE}
+else
+    echo "Postgres is not running: ${POSTGRES_MODULE} will not be tested."
+    sage -t ${PACKAGE_EXCEPT_POSTGRES}
+fi
