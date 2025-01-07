5536f7d2567d23f89f4dd54020d2edd74230a181
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2025-01-07 00:21:16 -0800

    setup.py: Define extras

--- setup.py
+++ setup.py
@@ -21,12 +21,39 @@ setuptools.setup(
     url="https://github.com/penguian/Boolean-Cayley-graphs",
     packages=["boolean_cayley_graphs"],
     exclude=["CAST-128","doc","nectar","papers-talks","pbs","sage-code","sobj"],
-    install_requires=["numpy","psycopg2","sagemath"],
+    install_requires=[
+        "numpy",
+    ],
     dependency_links=["https://pypi.org/project/"],
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
+    extras_require={
+        # Use one of these two
+        "psycopg2": [
+            "psycopg2",
+        ],
+        "psycopg2-binary": [
+            "psycopg2-binary",
+        ],
+        # Use one of these two
+        "sagemath": [
+            #"sagemath-standard",
+        ],
+        "passagemath": [
+            "passagemath-brial",
+            "passagemath-cliquer",
+            "passagemath-flint",
+            "passagemath-gap",
+            "passagemath-graphs",
+            "passagemath-modules",
+            "passagemath-pari",
+            "passagemath-plot",
+            "passagemath-repl",
+            "passagemath-symbolics",
+        ],
+    },
 )
 
